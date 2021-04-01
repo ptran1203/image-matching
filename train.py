087041e343a4b343c7044eb2fee73fccd103f224
phattran <phat.tranhoang@cyberlogitec.com>
2021-04-01 16:49:17 +0700

    acc

--- train.py
+++ train.py
@@ -64,8 +64,11 @@ def train_epoch(model, loader, optimizer, criterion):
 
     model.train()
     train_loss = []
+
     bar = tqdm(loader)
     for (data, target) in bar:
+        PREDS_M = []
+        TARGETS = []
 
         data, target = data.cuda(), target.cuda()
         optimizer.zero_grad()
@@ -75,12 +78,18 @@ def train_epoch(model, loader, optimizer, criterion):
         loss.backward()
         optimizer.step()
 
-        torch.cuda.synchronize()
-            
+        lmax_m = logits_m.max(1)
+        preds_m = lmax_m.indices
+
+        PREDS_M.append(preds_m.detach().cpu())
+        TARGETS.append(target.detach().cpu())
+
         loss_np = loss.detach().cpu().numpy()
         train_loss.append(loss_np)
         smooth_loss = sum(train_loss[-100:]) / min(len(train_loss), 100)
-        bar.set_description('loss: %.5f, smth: %.5f' % (loss_np, smooth_loss))
+        acc_m = (PREDS_M == TARGETS.numpy()).mean() * 100
+
+        bar.set_description('loss: %.5f, smth: %.5f, acc: %.5f' % (loss_np, smooth_loss, acc_m))
 
     return train_loss
 
@@ -114,7 +123,7 @@ def val_epoch(model, valid_loader, criterion):
         PREDS_M = torch.cat(PREDS_M).numpy()
         TARGETS = torch.cat(TARGETS)
 
-    acc_m = (PREDS_M == TARGETS.numpy()).mean() * 100.
+    acc_m = (PREDS_M == TARGETS.numpy()).mean() * 100
     y_true = {idx: target if target >=0 else None for idx, target in enumerate(TARGETS)}
     y_pred_m = {idx: (pred_cls, conf) for idx, (pred_cls, conf) in enumerate(zip(PREDS_M, PRODS_M))}
     val_f1_score = f1_score(y_true, y_pred_m)
