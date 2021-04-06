a0eac59a7ceaade9f29ba511cc52c19f2bb61879
phattran <phat.tranhoang@cyberlogitec.com>
2021-04-06 16:40:26 +0700

    try

--- models.py
+++ models.py
@@ -177,3 +177,19 @@ class ResNest101(nn.Module):
         logits_m = self.metric_classify(x)
 
         return l2_norm(x, axis=-1), logits_m
+
+
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6):
+        super(GeM,self).__init__()
+        self.p = nn.Parameter(torch.ones(1)*p)
+        self.eps = eps
+
+    def forward(self, x):
+        return self.gem(x, p=self.p, eps=self.eps)
+        
+    def gem(self, x, p=3, eps=1e-6):
+        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
+        
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
