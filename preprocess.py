eb39ed1911dce483720e81bb7c4b4f2d79721df3
phattran <phat.tranhoang@cyberlogitec.com>
2021-04-15 15:17:54 +0700

    imporGroupKFold

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import train_test_split, StratifiedKFold, GroupKFold
 from sklearn.preprocessing import LabelEncoder
 import argparse
 
