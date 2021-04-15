d4f8faa243f6b337b2fb535ba7b3646f8beece63
phattran <phat.tranhoang@cyberlogitec.com>
2021-04-15 09:14:46 +0700

    ifelse

--- models.py
+++ models.py
@@ -179,6 +179,7 @@ class EnsembleModels(nn.Module):
             self.out_dim = self.get_outdim(weight_path)
             # overring outdim
             
+        else:
             weight_path = weight_path.split("outdim") + f"outdim{self.out_dim}.pth"
 
         print(weight_path)
